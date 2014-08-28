27de61bc605a0d82a2bdd6408934f31a33465b49
Koboldthegreat <simontje.bil@gmail.com>
2014-08-28 18:32:33 +0200

    Doesn't work yet. Added some test functions. To-Do: implement it to use it with the speach recognition client.

--- client/modules/Youtube.py
+++ client/modules/Youtube.py
@@ -25,22 +25,16 @@ def youtube_play(id):
 	os.system("youtube-dl --id -x " + url)
 	os.system("mpv " + id + ".m4a")
 
-def youtube_search(options):
+def youtube_search(quote, max):
   youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
   # Call the search.list method to retrieve results matching the specified
   # query term.
   search_response = youtube.search().list(
-    q=options.q,
+    q=quote,
     part="id,snippet",
-    maxResults=options.max_results
-  ).execute()
-
-  
-  channels = []
-  playlists = []
-  
+    maxResults = max).execute()
 
   # Add each result to the appropriate list, and then display the lists of
   # matching videos, channels, and playlists.
@@ -50,29 +44,20 @@ def youtube_search(options):
           videos.append("%s" % (search_result["snippet"]["title"]))
           # videos.append("%s (%s)" % (search_result["snippet"]["title"], search_result["id"]["videoId"]))
           videosdict[search_result["snippet"]["title"]] = search_result["id"]["videoId"]
-    elif search_result["id"]["kind"] == "youtube#channel":
-		  channels.append("%s (%s)" % (search_result["snippet"]["title"],
-                                   search_result["id"]["channelId"]))
-    elif search_result["id"]["kind"] == "youtube#playlist":
-		  playlists.append("%s (%s)" % (search_result["snippet"]["title"],
-                                    search_result["id"]["playlistId"]))
+   
 
   
   for index, x in enumerate(videos):
 	  print index, x
-  
-  #print "Videos:\n", "\n".join(videos), "\n"
-  #print "Channels:\n", "\n".join(channels), "\n"
-  #print "Playlists:\n", "\n".join(playlists), "\n"
- 
 
 if __name__ == "__main__":
   argparser.add_argument("--q", help="Search term", default="Google")
   argparser.add_argument("--max-results", help="Max results", default=25)
   args = argparser.parse_args()
-  
+ 
+  quote = raw_input("Give the video name ")
   try:
-    youtube_search(args)
+    youtube_search(quote, 25)
   except HttpError, e:
     print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
   
